b62d0a37988432633d2d0b7072a3d7efe151a957
gabriel.leles18 <gabriel.leles18@gamil.com>
2019-03-20 15:48:44 -0300

    Views serviço atualizada

--- views/servico.php
+++ views/servico.php
@@ -22,30 +22,30 @@
         </div>
     </div>
     <div class="form-row">
-        <div class="form-group col-md-6">
+        <div class="form-group col-md-4">
             <label for="inputState">Tipo de Serviço:</label>
             <select id="inputState" class="form-control" name="tipo">
                 <option></option>
             </select>
         </div>
-        <div class="form-group col-md-6">
-            <label for="inputAddress">Horário de Agendamento:</label>
-            <input type="text" class="form-control" id="inputAddress" placeholder="" name="agendamento">
-        </div>
-    </div>
-    <div class="form-row">
-        <div class="form-group col-md-6">
+        <div class="form-group col-md-4">
             <label for="inputState">Técnico:</label>
             <select id="inputState" class="form-control" name="tecnico">
                 <option></option>
             </select>
         </div>
-        <div class="form-group col-md-6">
-            <label for="inputState">Descrição:</label>
-            <textarea class="form-control" rows="1" id="comment" name="descricao"></textarea>
+        <div class="form-group col-md-4">
+            <label for="inputAddress">Horário de Agendamento:</label>
+            <input type="text" class="form-control" id="inputAddress" placeholder="" name="agendamento">
         </div>
     </div>
     <div class="form-row">
-        <button type="submit" class="btn btn-primary">Finalizar</button>
+        <div class="form-group col-md-8">
+            <label for="inputState">Descrição:</label>
+            <textarea class="form-control" rows="3" id="comment" name="descricao"></textarea>
+        </div>
+    </div>
+    <div class="form-row">
+        <button type="submit" class="btn btn-primary">Salvar</button>
     </div>
 </form>
